27499efbe04007401c43e77e46685375aa2a3413
timo <bandit@local>
2019-08-25 18:15:16 +0300

    first draft

---
+++ src/main/java/com/example/samplemavenproject/SampleMavenProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.samplemavenproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleMavenProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleMavenProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/samplemavenproject/SampleMavenProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.samplemavenproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SampleMavenProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
